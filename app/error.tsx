975850cfc5f5fbcff996acaa94352a5d6e164808
NoistNT <arielgnr23@gmail.com>
2024-06-10 15:27:21 -0300

    Add error.tsx

---
+++ app/error.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function ErrorPage({ error }: { error: Error }) {
+  if (error instanceof Error) return <div>{error.message}</div>
+
+  return <div>Something went wrong</div>
+}
