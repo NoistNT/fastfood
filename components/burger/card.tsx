12bb864a8c5722677b3caa28ece1947ad2e72f0a
NoistNT <arielgnr23@gmail.com>
2024-06-15 18:15:50 -0300

    Update css in card component

--- components/burger/card.tsx
+++ components/burger/card.tsx
@@ -1,14 +1,6 @@
-import Image from 'next/image'
+import type { Burger } from '@/lib/types'
 
-interface Props {
-  name: string
-  description: string
-  imgAlt: string
-  imgSrc: string
-  isVegetarian: boolean
-  isVegan: boolean
-  price: number
-}
+import Image from 'next/image'
 
 export default function Card({
   name,
@@ -18,51 +10,48 @@ export default function Card({
   isVegetarian,
   isVegan,
   price
-}: Props) {
+}: Burger) {
   return (
-    <article className="max-w-md rounded-3xl border-2 border-neutral-300 dark:border-neutral-700">
+    <article className="h-full max-w-md rounded-xl border-2 border-neutral-400 dark:border-neutral-700 dark:bg-black">
       <Image
         alt={imgAlt}
-        className="rounded-b-sm rounded-t-3xl"
+        className="rounded-t-xl"
         height={480}
         src={imgSrc}
         width={480}
       />
-      <div className="h-1 w-full bg-neutral-300 dark:bg-neutral-800" />
-      <div className="p-4">
-        <div className="flex items-center justify-between">
+      <div className="h-1 w-full bg-neutral-400 dark:bg-neutral-600" />
+      <div className="mx-4 mt-4">
+        <div className="mb-4 flex items-center justify-between px-2">
           <h2 className="text-2xl font-bold">{name}</h2>
-          <p className="mt-0.5 text-xl font-semibold text-neutral-600 dark:text-neutral-300 lg:mr-3">
+          <p className="text-xl font-semibold text-neutral-600 dark:text-neutral-300">
             {' '}
             ${price}
           </p>
         </div>
-        <p className="px-1 text-neutral-500/85 dark:text-neutral-400/85">
+        <div className="mb-4 h-0.5 w-full bg-neutral-400 dark:bg-neutral-700" />
+        <p className="mb-5 min-h-16 px-4 text-neutral-500/85 dark:text-neutral-400/85">
           {description}
         </p>
-        <div className="mb-4 mt-10 h-0.5 w-full bg-neutral-200 dark:bg-neutral-700" />
-        <div className="flex justify-around py-0.5">
-          <div className="flex gap-2 font-semibold text-neutral-400">
-            <p>Vegetarian: </p>
-            <p>
-              {!isVegetarian ? (
-                <span className="text-green-500">Yes</span>
-              ) : (
-                <span className="text-red-400">No</span>
-              )}
-            </p>
-          </div>
-          <div className="flex gap-2 font-semibold text-neutral-400">
-            <p>Vegan:</p>
-            <p>
-              {isVegan ? (
-                <span className="text-green-500">Yes</span>
-              ) : (
-                <span className="text-red-400">No</span>
-              )}
-            </p>
-          </div>
-        </div>
+        <div className="mb-2 h-0.5 w-full bg-neutral-400 dark:bg-neutral-700" />
+        <ul className="mx-auto my-3 flex items-center justify-around text-center text-muted-foreground">
+          <p className="text-center font-semibold">
+            Vegetarian:
+            {!isVegetarian ? (
+              <span className="text-green-600"> Yes</span>
+            ) : (
+              <span className="text-red-400"> No</span>
+            )}
+          </p>
+          <p className="text-center font-semibold">
+            Vegan:
+            {isVegan ? (
+              <span className="text-green-600"> Yes</span>
+            ) : (
+              <span className="text-red-400"> No</span>
+            )}
+          </p>
+        </ul>
       </div>
     </article>
   )
