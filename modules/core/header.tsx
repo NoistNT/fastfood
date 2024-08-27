f776408adf3f19709e204adb76226b74fc47bb82
NoistNT <arielgnr23@gmail.com>
2024-08-27 16:54:24 -0300

    Add header component

--- modules/core/header.tsx
+++ modules/core/header.tsx
@@ -1,10 +1,15 @@
 import Link from 'next/link'
 
+import { Button } from '@/modules/core/ui/button'
+
 export default function Header() {
   return (
-    <header className="flex items-center justify-between bg-neutral-800 p-4 text-white">
+    <header className="flex items-center justify-between bg-black px-4 py-3.5 text-white transition-colors ease-in-out">
       <Link href="/products">
         <h1 className="text-2xl font-bold hover:text-neutral-300">Fastfood</h1>
+      </Link>
+      <Link className="mr-[4.5rem] sm:mr-20" href="/order">
+        <Button variant="secondary">Ver pedido</Button>
       </Link>
     </header>
   )
