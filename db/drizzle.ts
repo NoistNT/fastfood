865070c388869bea295e01653bcd51fb17557067
NoistNT <arielgnr23@gmail.com>
2024-08-28 12:28:00 -0300

    Refactor in drizzle config

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,11 +1,9 @@
 import { neon } from '@neondatabase/serverless'
-import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/neon-http'
 
+import { DB_URL } from '@/constants'
 import * as schema from '@/db/schema'
 
-config({ path: './.env.local' })
-
-const sql = neon(process.env.DB_URL!)
+const sql = neon(DB_URL)
 
 export const db = drizzle(sql, { schema })
