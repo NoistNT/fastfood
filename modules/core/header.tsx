97854cd694420c59488f2ae37bf8f3b5aae5b066
NoistNT <arielgnr23@gmail.com>
2024-08-28 12:29:19 -0300

    Fix route in header component

--- modules/core/header.tsx
+++ modules/core/header.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/modules/core/ui/button'
 export default function Header() {
   return (
     <header className="flex items-center justify-between bg-black px-4 py-3.5 text-white transition-colors ease-in-out">
-      <Link href="/products">
+      <Link href="/">
         <h1 className="text-2xl font-bold hover:text-neutral-300">Fastfood</h1>
       </Link>
       <Link className="mr-[4.5rem] sm:mr-20" href="/order">
