d968446e945e250c9d2662da8ea3a954cc94ee86
NoistNT <arielgnr23@gmail.com>
2024-08-30 21:53:46 -0300

    Add use order hook

--- store/use-order.ts
+++ store/use-order.ts
@@ -1,58 +1,21 @@
+import type { Order } from '@/modules/orders/types'
+
 import { create } from 'zustand'
 
-interface Item {
-  id: number
-  name: string
-  price: number
-  quantity: number
-}
-
 interface OrderStore {
-  order: Item[]
-  addItem: (item: Item) => void
-  incrementQuantity: (id: number) => void
-  decrementQuantity: (id: number) => void
-  removeItem: (id: number) => void
-  clearOrder: () => void
+  order: Order
+  orders: Order[]
+  addOrder: (order: Order) => void
 }
 
 export const useOrderStore = create<OrderStore>((set) => ({
-  order: [],
-  addItem: (item) =>
-    set((state) => {
-      const existingItemIndex = state.order.findIndex(
-        (orderItem) => orderItem.id === item.id
-      )
-
-      if (existingItemIndex > -1) {
-        const updatedOrder = [...state.order]
-
-        updatedOrder[existingItemIndex].quantity += item.quantity
-
-        return { order: updatedOrder }
-      }
-
-      return { order: [...state.order, item] }
-    }),
-  incrementQuantity: (id) =>
-    set((state) => {
-      const updatedOrder = state.order.map((item) =>
-        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
-      )
-
-      return { order: updatedOrder }
-    }),
-  decrementQuantity: (id) =>
-    set((state) => {
-      const updatedOrder = state.order.map((item) =>
-        item.id === id && item.quantity > 1
-          ? { ...item, quantity: item.quantity - 1 }
-          : item
-      )
-
-      return { order: updatedOrder }
-    }),
-  removeItem: (id) =>
-    set((state) => ({ order: state.order.filter((item) => item.id !== id) })),
-  clearOrder: () => set({ order: [] })
+  order: {
+    id: 0,
+    createdAt: new Date(),
+    total: 0,
+    items: [],
+    status: 'PENDING'
+  },
+  orders: [],
+  addOrder: (order) => set({ order })
 }))
