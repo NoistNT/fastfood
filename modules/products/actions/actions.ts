98750c11db0042c409843a58d76715cf885dc6c0
NoistNT <arielgnr23@gmail.com>
2024-09-21 12:21:40 -0300

    Add products server actions

---
+++ modules/products/actions/actions.ts
@@ -0,0 +1,48 @@
+'use server'
+
+import type {
+  ProductGeneralView,
+  ProductWithIngredients
+} from '@/modules/products/types'
+
+import { eq } from 'drizzle-orm'
+
+import { db } from '@/db/drizzle'
+import { products } from '@/db/schema'
+
+export const findAll = async (): Promise<ProductGeneralView[]> => {
+  return await db.query.products.findMany({
+    columns: {
+      id: true,
+      name: true,
+      description: true,
+      imgAlt: true,
+      imgSrc: true,
+      price: true,
+      isAvailable: true
+    }
+  })
+}
+
+export const findOne = async (
+  id: number
+): Promise<ProductWithIngredients | null> => {
+  const productWithIngredients = await db.query.products.findFirst({
+    where: eq(products.id, id),
+    with: {
+      ingredients: {
+        columns: {},
+        with: { ingredient: { columns: { name: true } } }
+      }
+    }
+  })
+
+  if (!productWithIngredients) return null
+
+  const { ingredients, ...product } = productWithIngredients
+
+  return {
+    ...product,
+    ingredients: ingredients.map(({ ingredient }) => ingredient.name)
+  }
+}
