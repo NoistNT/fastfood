cb48021c091e0fbd7e076cccb13d0ce6d0d227e3
NoistNT <arielgnr23@gmail.com>
2024-09-22 10:01:15 -0300

    Add metadata and static params to product page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,16 +1,39 @@
-import { findOne } from '@/modules/products/actions/actions'
+import { BASE_URL } from '@/constants'
+import { findAll, findOne } from '@/modules/products/actions/actions'
 import DetailCard from '@/modules/products/components/detail-card'
 
 interface Props {
   params: { id: number }
 }
 
-export default async function Page({ params }: Props) {
-  const { id } = params
-
+export async function generateMetadata({ params: { id } }: Props) {
   const product = await findOne(id)
 
-  if (!product) return null
+  if (!product) {
+    return {
+      title: 'Fast Food',
+      description: 'The product you are looking for does not exist.',
+      metadataBase: new URL(BASE_URL)
+    }
+  }
+
+  return {
+    title: `FastFood - ${product.name} `,
+    description: product.description,
+    metadataBase: new URL(BASE_URL)
+  }
+}
+
+export async function generateStaticParams() {
+  const products = await findAll()
+
+  return products.map(({ id }) => ({ id: String(id) }))
+}
+
+export default async function Page({ params: { id } }: Props) {
+  const product = await findOne(id)
+
+  if (!product) return <p>Product not found</p>
 
   return <DetailCard product={product} />
 }
