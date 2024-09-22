1d349c541eed064e7f8cf4f0b4045305f790b09c
NoistNT <arielgnr23@gmail.com>
2024-09-22 20:10:41 -0300

    Refactor submit-order component

--- modules/orders/components/submit-order.tsx
+++ modules/orders/components/submit-order.tsx
@@ -1,30 +1,35 @@
+import type { MouseEventHandler } from 'react'
+
 import Link from 'next/link'
 
 import { Button } from '@/modules/core/ui/button'
 
 interface Props {
-  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+  handleSubmit: MouseEventHandler<HTMLButtonElement>
   isPending: boolean
 }
 
 export function SubmitOrder({ handleSubmit, isPending }: Props) {
   return (
-    <form
-      className="flex items-center justify-end gap-2 py-1.5"
-      onSubmit={handleSubmit}
-    >
+    <div className="flex items-center justify-end gap-2 py-1.5">
       <Link href="/products">
         <Button
-          className="border border-neutral-300 hover:border-neutral-400 dark:h-10 dark:border-neutral-700 dark:hover:border-neutral-600"
+          className="border border-neutral-300 hover:border-neutral-400 dark:border-neutral-700 dark:hover:border-neutral-600"
           type="button"
           variant="secondary"
         >
           Seguir agregando
         </Button>
       </Link>
-      <Button aria-disabled={isPending} type="submit" variant="default">
-        {isPending ? 'Registrando pedido...' : 'Confirmar pedido'}
+      <Button
+        aria-disabled={isPending}
+        className="dark:border dark:border-neutral-900"
+        type="submit"
+        variant="default"
+        onClick={handleSubmit}
+      >
+        {isPending ? 'Registrando pedido' : 'Confirmar pedido'}
       </Button>
-    </form>
+    </div>
   )
 }
