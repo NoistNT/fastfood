5becd31ea43b8d3960fb0febf005daa4c8575dac
NoistNT <arielgnr23@gmail.com>
2024-09-22 20:09:11 -0300

    Add new methods (create, updateStatus) and guards (can transition) to order actions

--- modules/orders/actions/actions.ts
+++ modules/orders/actions/actions.ts
@@ -1,13 +1,59 @@
 'use server'
 
+import type { NewOrder, OrderStatus } from '@/modules/orders/types'
+
+import { eq } from 'drizzle-orm'
+
 import { db } from '@/db/drizzle'
-import { orders } from '@/db/schema'
+import { orderItem, orders } from '@/db/schema'
+import { ORDER_STATUS } from '@/modules/orders/types'
 
-export const create = async (formData: FormData) => {
-  const order = {
-    total: Number(formData.get('total')),
-    createdAt: new Date()
-  }
+export const create = async (newOrder: NewOrder) => {
+  const orderId = await db
+    .insert(orders)
+    .values(newOrder)
+    .returning({ id: orders.id })
 
-  await db.insert(orders).values(order)
+  await db.insert(orderItem).values(
+    newOrder.items.map((item) => ({
+      orderId: orderId[0].id,
+      productId: item.id,
+      quantity: item.quantity
+    }))
+  )
+}
+
+const statusTransitions: Record<OrderStatus, OrderStatus[]> = {
+  [ORDER_STATUS.PENDING]: [ORDER_STATUS.PROCESSING],
+  [ORDER_STATUS.PROCESSING]: [ORDER_STATUS.SHIPPED],
+  [ORDER_STATUS.SHIPPED]: [ORDER_STATUS.DELIVERED],
+  [ORDER_STATUS.DELIVERED]: []
+}
+
+const canTransition = (currentStatus: OrderStatus, newStatus: OrderStatus) => {
+  return statusTransitions[currentStatus].includes(newStatus)
+}
+
+const isValidStatus = (status: OrderStatus): status is OrderStatus => {
+  return Object.values(ORDER_STATUS).includes(status)
+}
+
+export const updateStatus = async (orderId: string, newStatus: OrderStatus) => {
+  if (!isValidStatus(newStatus)) throw new Error('Invalid status')
+
+  const order = await db.query.orders.findFirst({
+    where: eq(orders.id, orderId)
+  })
+
+  if (!order) throw new Error('Order not found')
+
+  if (!canTransition(order.status as OrderStatus, newStatus))
+    throw new Error('Invalid transition')
+
+  const result = await db
+    .update(orders)
+    .set({ status: newStatus })
+    .where(eq(orders.id, orderId))
+
+  return result
 }
