8a567a6574a13d6460da2192f18c4d1364a175b0
NoistNT <arielgnr23@gmail.com>
2024-09-23 17:08:56 -0300

    Add order page

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,5 +1,44 @@
-import Order from '@/modules/orders/components/order'
+'use client'
+
+import { useMemo, useTransition } from 'react'
+
+import { EmptyOrder } from '@/modules/orders/components/empty-order'
+import { OrderTable } from '@/modules/orders/components/order-table'
+import { SubmitOrder } from '@/modules/orders/components/submit-order'
+import { calculateTotal, submitOrder } from '@/modules/orders/utils'
+import { useOrderStore } from '@/store/use-order'
 
 export default function Page() {
-  return <Order />
+  const {
+    items,
+    incrementQuantity,
+    decrementQuantity,
+    removeItem,
+    clearOrder
+  } = useOrderStore()
+
+  const total = useMemo(() => calculateTotal(items), [items])
+
+  const [isPending, startTransition] = useTransition()
+
+  const handleSubmit = async () => {
+    startTransition(async () => {
+      await submitOrder(items, total, clearOrder)
+    })
+  }
+
+  if (!items.length) return <EmptyOrder />
+
+  return (
+    <div className="mx-auto max-w-5xl">
+      <OrderTable
+        decrementQuantity={decrementQuantity}
+        incrementQuantity={incrementQuantity}
+        items={items}
+        removeItem={removeItem}
+        total={total}
+      />
+      <SubmitOrder handleSubmit={handleSubmit} isPending={isPending} />
+    </div>
+  )
 }
