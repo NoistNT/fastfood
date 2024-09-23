451e26c4de038a7d8fa2e074e2afe9d077debf72
NoistNT <arielgnr23@gmail.com>
2024-09-23 17:03:56 -0300

    Move orders method from util to orders utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,7 +4,3 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export function fixedPrice(price: number) {
-  return Number(price.toFixed(2))
-}
