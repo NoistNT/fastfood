69dae784cc20487fdd21f9dd43775f9fdd376bfe
NoistNT <arielgnr23@gmail.com>
2024-09-23 10:33:26 -0300

    Add methods: additem, increment and decrement quantity, removeItem and clearOrder to use-order store

--- store/use-order.ts
+++ store/use-order.ts
@@ -1,21 +1,55 @@
-import type { Order } from '@/modules/orders/types'
+import type { Item } from '@/modules/orders/types'
 
 import { create } from 'zustand'
 
 interface OrderStore {
-  order: Order
-  orders: Order[]
-  addOrder: (order: Order) => void
+  items: Item[]
+  addItem: (item: Item) => void
+  incrementQuantity: (id: number) => void
+  decrementQuantity: (id: number) => void
+  removeItem: (id: number) => void
+  clearOrder: () => void
 }
 
 export const useOrderStore = create<OrderStore>((set) => ({
-  order: {
-    id: 0,
-    createdAt: new Date(),
-    total: 0,
-    items: [],
-    status: 'PENDING'
-  },
-  orders: [],
-  addOrder: (order) => set({ order })
+  items: [],
+  addItem: (item) =>
+    set((state) => {
+      const existingItemIndex = state.items.findIndex(
+        (orderItem) => orderItem.id === item.id
+      )
+
+      if (existingItemIndex > -1) {
+        const updatedOrder = [...state.items]
+
+        updatedOrder[existingItemIndex].quantity += item.quantity
+
+        return { items: updatedOrder }
+      }
+
+      return { items: [...state.items, item] }
+    }),
+  incrementQuantity: (id) =>
+    set((state) => {
+      const updatedOrder = state.items.map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+      )
+
+      return { items: updatedOrder }
+    }),
+  decrementQuantity: (id) =>
+    set((state) => {
+      const updatedOrder = state.items.map((item) =>
+        item.id === id && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      )
+
+      return { items: updatedOrder }
+    }),
+  removeItem: (id) =>
+    set((state) => ({
+      items: state.items.filter((item) => item.id !== id)
+    })),
+  clearOrder: () => set({ items: [] })
 }))
