d1ac2ad7ee23f75bc89936535b417b0ae3977563
NoistNT <arielgnr23@gmail.com>
2024-09-25 10:44:37 -0300

    Add create action and try-catch blocks for better error handling in orders module

--- modules/orders/actions/actions.ts
+++ modules/orders/actions/actions.ts
@@ -1,6 +1,11 @@
 'use server'
 
-import type { NewOrder, OrderStatus } from '@/modules/orders/types'
+import type {
+  FindManyResponse,
+  NewOrder,
+  OrderStatus,
+  OrderWithItems
+} from '@/modules/orders/types'
 
 import { eq } from 'drizzle-orm'
 
@@ -8,67 +13,94 @@ import { db } from '@/db/drizzle'
 import { orderItem, orders } from '@/db/schema'
 import { canTransition, isValidStatus } from '@/modules/orders/utils'
 
-export const create = async (newOrder: NewOrder) => {
-  const orderId = await db
-    .insert(orders)
-    .values(newOrder)
-    .returning({ id: orders.id })
+const createItem = async (orderId: string, newOrder: NewOrder) => {
+  try {
+    await db.insert(orderItem).values(
+      newOrder.items.map(({ id, quantity }) => ({
+        orderId,
+        productId: id,
+        quantity
+      }))
+    )
+  } catch (error) {
+    const err = error as Error
 
-  await db.insert(orderItem).values(
-    newOrder.items.map((item) => ({
-      orderId: orderId[0].id,
-      productId: item.id,
-      quantity: item.quantity
-    }))
-  )
+    throw new Error(
+      `No se pudo crear el item para la orden ${orderId}: ${err.message}`
+    )
+  }
 }
 
-export const findAll = async () => {
-  const res = await db.query.orders.findMany({
-    with: {
-      orderItems: {
-        columns: { quantity: true },
-        with: { product: { columns: { name: true, price: true } } }
-      }
-    }
-  })
+export const create = async (newOrder: NewOrder) => {
+  try {
+    const orderId = await db
+      .insert(orders)
+      .values(newOrder)
+      .returning({ id: orders.id })
 
-  const orders = res.map(({ id, status, total, createdAt, orderItems }) => {
-    if (!isValidStatus(status)) {
-      throw new Error('Invalid status')
-    }
+    await createItem(orderId[0].id, newOrder)
+  } catch (error) {
+    throw new Error('No se pudo registrar el pedido. Intente nuevamente')
+  }
+}
+
+export const ordersList = (orders: FindManyResponse[]): OrderWithItems[] => {
+  return orders.map((order) => {
+    if (!isValidStatus(order.status)) throw new Error('Invalid status')
 
     return {
-      id,
-      total,
-      status,
-      createdAt,
-      items: orderItems.map(({ product: { name, price }, quantity }) => ({
+      order,
+      items: order.orderItems.map(({ product: { name, price }, quantity }) => ({
         name,
         quantity,
         subtotal: quantity * price
       }))
     }
   })
+}
 
-  return orders
+export const findAll = async () => {
+  try {
+    const orders: FindManyResponse[] = await db.query.orders.findMany({
+      with: {
+        orderItems: {
+          columns: { quantity: true },
+          with: { product: { columns: { name: true, price: true } } }
+        }
+      }
+    })
+
+    return ordersList(orders)
+  } catch (error) {
+    throw new Error(
+      'No se pudo obtener la lista de pedidos. Intente nuevamente'
+    )
+  }
 }
 
 export const updateStatus = async (orderId: string, newStatus: OrderStatus) => {
-  if (!isValidStatus(newStatus)) throw new Error('Invalid status')
+  try {
+    if (!isValidStatus(newStatus)) throw new Error('Invalid status')
 
-  const order = await db.query.orders.findFirst({
-    where: eq(orders.id, orderId)
-  })
+    const order = await db.query.orders.findFirst({
+      where: eq(orders.id, orderId)
+    })
 
-  if (!order) throw new Error('Order not found')
+    if (!order) throw new Error('Order not found')
 
-  if (!canTransition(order.status as OrderStatus, newStatus)) {
-    throw new Error('Invalid transition')
+    if (!canTransition(order.status as OrderStatus, newStatus)) {
+      throw new Error('Invalid transition')
+    }
+
+    return await db
+      .update(orders)
+      .set({ status: newStatus })
+      .where(eq(orders.id, orderId))
+  } catch (error) {
+    const err = error as Error
+
+    throw new Error(
+      `No se pudo actualizar el estado de la orden ${orderId}: ${err.message}`
+    )
   }
-
-  return await db
-    .update(orders)
-    .set({ status: newStatus })
-    .where(eq(orders.id, orderId))
 }
