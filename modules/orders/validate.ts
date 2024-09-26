94057d93af41930126b8f9ea71a105f7630981aa
NoistNT <arielgnr23@gmail.com>
2024-09-26 15:35:30 -0300

    Add validateData generic function and OrderId zod schema in orders module

--- modules/orders/validate.ts
+++ modules/orders/validate.ts
@@ -20,13 +20,13 @@ export const isValidStatus = (status: string): status is OrderStatus => {
   return Object.values(ORDER_STATUS).includes(status as OrderStatus)
 }
 
+export const OrderId = z.string().uuid('Invalid order ID')
+
 export const CreateItem = z.object({
-  orderId: z.string(),
+  orderId: OrderId,
   productId: z.number(),
   quantity: z.number().min(1, 'Quantity must be at least 1')
 })
-
-export const CreateOrderId = z.object({ id: z.string() })
 
 export const CreateNewOrder = z.object({
   items: z
@@ -36,6 +36,14 @@ export const CreateNewOrder = z.object({
         quantity: z.number().min(1, 'Quantity must be at least 1')
       })
     )
-    .nonempty(),
+    .nonempty('Order must have at least one item'),
   total: z.number().positive('Order total must be a positive value')
 })
+
+export const validateData = <T>(schema: z.ZodSchema<T>, data: unknown): T => {
+  const { data: validatedData, error, success } = schema.safeParse(data)
+
+  if (!success) throw new Error(error.issues[0].message, { cause: error })
+
+  return validatedData
+}
