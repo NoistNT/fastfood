f5c01e79534a67de4bb0a5424443878864f4aed3
NoistNT <arielgnr23@gmail.com>
2024-11-02 18:53:06 -0300

    Update to next 15

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }]
   }
