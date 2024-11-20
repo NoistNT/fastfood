755ca71fc57b51307e6e2289a7624f0c2a70afe0
NoistNT <arielgnr23@gmail.com>
2024-11-20 19:20:34 -0300

    Update params to work in next js 15

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -6,21 +6,22 @@ interface Props {
   params: { id: number }
 }
 
-export async function generateMetadata({ params: { id } }: Props) {
+export async function generateMetadata({ params }: Props) {
+  const { id } = await params
   const product = await findOne(id)
 
   if (!product) {
     return {
       title: 'Fast Food',
       description: 'The product you are looking for does not exist.',
-      metadataBase: new URL(BASE_URL)
+      metadataBase: new URL(BASE_URL),
     }
   }
 
   return {
     title: `FastFood - ${product.name} `,
     description: product.description,
-    metadataBase: new URL(BASE_URL)
+    metadataBase: new URL(BASE_URL),
   }
 }
 
@@ -30,7 +31,8 @@ export async function generateStaticParams() {
   return products.map(({ id }) => ({ id: String(id) }))
 }
 
-export default async function Page({ params: { id } }: Props) {
+export default async function Page({ params }: Props) {
+  const { id } = await params
   const product = await findOne(id)
 
   if (!product) return <p>Product not found</p>
