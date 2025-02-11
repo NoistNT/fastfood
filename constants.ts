9f89147dc9348061b0a5ed40673ff708f4e5056b
NoistNT <arielgnr23@gmail.com>
2025-02-11 20:12:19 -0300

    Update required environment variables to remove NEXT_PUBLIC prefix

--- constants.ts
+++ constants.ts
@@ -4,10 +4,10 @@ const missingEnvError = (key: string) => {
   throw new Error(`Missing environment variable: ${key}`);
 };
 
-const requiredEnvVars = ['DB_URL', 'NEXT_PUBLIC_BASE_URL'];
+const requiredEnvVars = ['DB_URL', 'BASE_URL'];
 
 requiredEnvVars.forEach((key) => {
   if (!process.env[key]) missingEnvError(key);
 });
 
-export const { DB_URL, NEXT_PUBLIC_BASE_URL: BASE_URL } = process.env as Record<string, string>;
+export const { DB_URL, BASE_URL } = process.env as Record<string, string>;
