53264a3424f892417687551e5359d17717dbfed9
NoistNT <arielgnr23@gmail.com>
2025-02-22 19:08:54 -0300

    Add utility function to convert strings to title case

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function toTitleCase(string: string) {
+  return string[0] + string.slice(1).toLowerCase();
+}
