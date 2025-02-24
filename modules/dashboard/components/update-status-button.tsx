7a869708f3acd8946df4a597daf0646e9520c3e1
NoistNT <arielgnr23@gmail.com>
2025-02-23 22:17:04 -0300

    Refactor UpdateStatusButton component to include currentStatus prop and improve button text formatting

--- modules/dashboard/components/update-status-button.tsx
+++ modules/dashboard/components/update-status-button.tsx
@@ -1,23 +1,33 @@
+import { toTitleCase } from '@/lib/utils';
 import { Button } from '@/modules/core/ui/button';
 
 interface Props {
   nextStatus: string;
+  currentStatus: string;
   isChangingStatus: boolean;
   handleUpdateStatus: () => void;
 }
 
-export function UpdateStatusButton({ nextStatus, isChangingStatus, handleUpdateStatus }: Props) {
+export function UpdateStatusButton({
+  nextStatus,
+  currentStatus,
+  isChangingStatus,
+  handleUpdateStatus,
+}: Props) {
   return (
-    <div className="mt-2 flex justify-end">
+    <div className="mt-2 flex justify-center">
       {nextStatus ? (
         <Button
-          className="min-w-32"
+          className="w-2/5 md:w-1/5 bg-background"
           disabled={isChangingStatus}
           type="button"
           variant={isChangingStatus ? 'secondary' : 'outline'}
+          size="sm"
           onClick={handleUpdateStatus}
         >
-          {isChangingStatus ? 'Updating...' : nextStatus}
+          {isChangingStatus
+            ? 'Updating...'
+            : toTitleCase(currentStatus) + ' -->  ' + toTitleCase(nextStatus)}
         </Button>
       ) : null}
     </div>
