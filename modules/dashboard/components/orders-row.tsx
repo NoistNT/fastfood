43e2964b3b9a6f44bc881798bfab78a030597179
NoistNT <arielgnr23@gmail.com>
2025-02-23 22:16:50 -0300

    Add gradients to tablerows in dashboard table

--- modules/dashboard/components/orders-row.tsx
+++ modules/dashboard/components/orders-row.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 
+import { toTitleCase } from '@/lib/utils';
 import { TableCell, TableRow } from '@/modules/core/ui/table';
 import { ExpandButton } from '@/modules/dashboard/components/expand-button';
 import { ExpandableRow } from '@/modules/dashboard/components/expandable-row';
@@ -17,6 +18,7 @@ export function OrdersRow({
     items,
   },
 }: OrderItemRowProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
   const [currentStatus, setCurrentStatus] = useState<OrderStatus>(status);
   const [statusTransitions, setStatusTransitions] = useState<StatusHistory[]>(
     statusHistory?.map(({ status, createdAt }) => ({
@@ -24,7 +26,6 @@ export function OrdersRow({
       createdAt: new Date(createdAt),
     })) || []
   );
-  const [isExpanded, setIsExpanded] = useState(false);
 
   const handleStatusUpdate = (newStatus: OrderStatus) => {
     setCurrentStatus(newStatus);
@@ -33,8 +34,8 @@ export function OrdersRow({
 
   return (
     <>
-      <TableRow className="bg-neutral-50 font-semibold text-neutral-700 hover:bg-neutral-100 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700">
-        <TableCell className="w-1/5">{currentStatus}</TableCell>
+      <TableRow className="bg-pink-50/40 hover:bg-gradient-to-r hover:from-pink-100/80 hover:to-violet-200/80 dark:bg-violet-950/10 dark:hover:from-indigo-950/70 dark:hover:to-purple-950/50 tracking-tight">
+        <TableCell className="w-1/5">{toTitleCase(currentStatus)}</TableCell>
         <TableCell className="w-1/5">${total}</TableCell>
         <TableCell className="w-1/5">{createdAt.toLocaleTimeString()}</TableCell>
         <TableCell className="w-1/5">{createdAt.toLocaleDateString()}</TableCell>
