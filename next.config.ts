eaea8da2ec660537507ded41f9a0c7ab8c4ecb36
NoistNT <arielgnr23@gmail.com>
2025-03-16 20:10:43 -0300

    Enhance next.config.ts with type definitions and experimental features for improved logging and React compiler support

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
-  },
+  images: { remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }] },
+  experimental: { reactCompiler: true },
+  logging: { fetches: { fullUrl: true } },
 };
 
 export default nextConfig;
