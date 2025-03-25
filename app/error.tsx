94c9fa23825fb31c32b99b5aaf48c5e160af1cfe
NoistNT <arielgnr23@gmail.com>
2025-03-25 17:36:36 -0300

    Improve error handling in ErrorPage component with toast notifications and enhanced UI

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,20 @@
 'use client';
 
-export default function ErrorPage({ error }: { error: Error }) {
-  if (error instanceof Error) return <div>{error.message}</div>;
+import { toast } from '@/modules/core/hooks/use-toast';
 
-  return <div>Something went wrong</div>;
+export default function ErrorPage({ error }: { error: Error }) {
+  if (error instanceof Error) {
+    toast({
+      title: 'Something went wrong',
+      description: error.message,
+      variant: 'destructive',
+    });
+    return (
+      <div className="flex flex-col gap-y-4 justify-center items-center w-full h-full text-muted-foreground">
+        <h1 className="text-xl font-medium">Something went wrong</h1>
+        <span className="text-sm">{error.message}</span>
+      </div>
+    );
+  }
+  return null;
 }
