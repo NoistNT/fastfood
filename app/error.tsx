0fd60502bc4c92cc2bc0b2a097fc1613419495bb
NoistNT <arielgnr23@gmail.com>
2025-10-28 20:55:23 +0000

    Improve error handling in global error

--- app/error.tsx
+++ app/error.tsx
@@ -1,18 +1,24 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
+import { useEffect } from 'react';
 
 import { toast } from '@/modules/core/hooks/use-toast';
 
 export default function ErrorPage({ error }: { error: Error }) {
   const t = useTranslations('Error');
 
+  useEffect(() => {
+    if (error instanceof Error) {
+      toast({
+        title: t('title'),
+        description: error.message,
+        variant: 'destructive',
+      });
+    }
+  }, [error, t]);
+
   if (error instanceof Error) {
-    toast({
-      title: t('title'),
-      description: error.message,
-      variant: 'destructive',
-    });
     return (
       <div className="flex flex-col gap-y-4 justify-center items-center w-full h-full text-muted-foreground">
         <h1 className="text-xl font-medium">{t('title')}</h1>
