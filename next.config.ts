14346562323201cdb2c3c360eb54f5aa8d5c0c84
NoistNT <arielgnr23@gmail.com>
2025-10-28 20:53:50 +0000

    Update to next 16

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: { remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }] },
-  experimental: { reactCompiler: true },
   logging: { fetches: { fullUrl: true } },
 };
 
