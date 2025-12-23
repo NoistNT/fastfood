18084883dace5afbbab1c4a854064ec3baa1331a
NoistNT <arielgnr23@gmail.com>
2025-12-23 13:57:51 +0000

    feat(i18n): add/enhance internationalization support

--- i18n/request.ts
+++ i18n/request.ts
@@ -2,7 +2,7 @@ import { getRequestConfig } from 'next-intl/server';
 import { headers } from 'next/headers';
 
 export default getRequestConfig(async () => {
-  const accept = (await headers()).get('accept-language') || '';
+  const accept = (await headers()).get('accept-language') ?? '';
   const locale = accept.startsWith('es') ? 'es' : 'en';
   return {
     locale,
